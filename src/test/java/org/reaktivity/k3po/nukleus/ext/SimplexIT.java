bb0f8eb15fcdcc8ac0aa0755ea9438d49c4a6ce9
NicoletaOita <nicoleta.oita@kaazing.com>
2017-10-06 16:03:22 +0300

    Updated issue number

--- src/test/java/org/reaktivity/k3po/nukleus/ext/SimplexIT.java
+++ src/test/java/org/reaktivity/k3po/nukleus/ext/SimplexIT.java
@@ -177,7 +177,7 @@ public class SimplexIT
         k3po.finish();
     }
 
-    @Ignore ("Please see issue #1206: https://github.com/kaazing/tickets/issues/1206")
+    @Ignore ("Please see issue #22: https://github.com/reaktivity/k3po-nukleus-ext.java/issues/22")
     @Test
     @Specification({
         "server.sent.throttle.initial.only.update.none/client",
