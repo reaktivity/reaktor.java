898d53f0c1f9ea9314b3493a575b98ff3ecf22a7
David Witherspoon <david.witherspoon@kaazing.com>
2017-12-22 16:08:54 -0800

    routes file saves Route entries

--- src/main/java/org/reaktivity/reaktor/internal/router/Router.java
+++ src/main/java/org/reaktivity/reaktor/internal/router/Router.java
@@ -21,31 +21,38 @@ import static org.reaktivity.reaktor.internal.router.RouteMatchers.sourceRefMatc
 import static org.reaktivity.reaktor.internal.router.RouteMatchers.targetMatches;
 import static org.reaktivity.reaktor.internal.router.RouteMatchers.targetRefMatches;
 
-import java.util.LinkedHashSet;
-import java.util.Optional;
-import java.util.Set;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 import org.agrona.DirectBuffer;
 import org.agrona.MutableDirectBuffer;
-import org.agrona.concurrent.UnsafeBuffer;
 import org.reaktivity.nukleus.Nukleus;
 import org.reaktivity.nukleus.function.MessageFunction;
 import org.reaktivity.nukleus.function.MessagePredicate;
 import org.reaktivity.reaktor.internal.Context;
+import org.reaktivity.reaktor.internal.layouts.RoutesLayout;
+import org.reaktivity.reaktor.internal.types.OctetsFW;
 import org.reaktivity.reaktor.internal.types.control.RouteFW;
 import org.reaktivity.reaktor.internal.types.control.UnrouteFW;
+import org.reaktivity.reaktor.internal.types.state.RouteEntryFW;
+import org.reaktivity.reaktor.internal.types.state.RouteTableFW;
 
 public final class Router extends Nukleus.Composite
 {
-    private final RouteFW routeRO = new RouteFW();
-
-    private final Set<RouteFW> routes;
+    private final RoutesLayout routesLayout;
+    private final RouteTableFW.Builder routeTableRW;
+    private final RouteTableFW routeTableRO;
+    private final MutableDirectBuffer routesBuffer;
+    private final int routesBufferCapacity;
 
     public Router(
         Context context)
     {
-        this.routes = new LinkedHashSet<>();
+        this.routesLayout = context.routesLayout();
+        this.routeTableRW = new RouteTableFW.Builder();
+        this.routeTableRO = new RouteTableFW();
+        this.routesBuffer = routesLayout.routesBuffer();
+        this.routesBufferCapacity = routesLayout.capacity();
     }
 
     @Override
@@ -58,13 +65,33 @@ public final class Router extends Nukleus.Composite
         RouteFW route,
         MessagePredicate routeHandler)
     {
-        DirectBuffer srcBuffer = route.buffer();
-        MutableDirectBuffer copyBuffer = new UnsafeBuffer(new byte[route.sizeof()]);
-        copyBuffer.putBytes(0, srcBuffer, route.offset(), copyBuffer.capacity());
-        RouteFW newRoute = new RouteFW().wrap(copyBuffer, 0, copyBuffer.capacity());
+        RouteTableFW routeTable = routeTableRO.wrap(routesBuffer, 0, routesBufferCapacity);
 
-        return routeHandler.test(route.typeId(), route.buffer(), route.offset(), route.sizeof()) &&
-               routes.add(newRoute);
+        final boolean routed = routeHandler.test(route.typeId(), route.buffer(), route.offset(), route.sizeof());
+
+        if (routed)
+        {
+            int readLock = routesLayout.lock();
+
+            routeTableRW
+                .wrap(routesBuffer, 0, routesBufferCapacity)
+                .writeLockAcquires(readLock)
+                .writeLockReleases(readLock--)
+                .routeEntries(b ->
+                {
+                    routeTable.routeEntries().forEach(
+                        e -> b.item(copyRouteEntry(e.routeSize(), e.route()))
+                    );
+
+                    b.item(
+                        routeEntryBuilder -> routeEntryBuilder.routeSize(route.sizeof())
+                                                              .route(routeBuilder -> copyRoute(routeBuilder, route))
+                    );
+                });
+
+            routesLayout.unlock();
+        }
+        return routed;
     }
 
     public boolean doUnroute(
@@ -84,36 +111,95 @@ public final class Router extends Nukleus.Composite
                 .and(targetRefMatches(targetRef))
                 .and(authorizationMatches(authorization));
 
-        return routes.removeIf(filter) &&
-               routeHandler.test(unroute.typeId(), unroute.buffer(), unroute.offset(), unroute.sizeof());
+        RouteTableFW routeTable = routeTableRO.wrap(routesBuffer, 0, routesBufferCapacity);
+        final boolean unrouted =
+            routeHandler.test(unroute.typeId(), unroute.buffer(), unroute.offset(), unroute.sizeof()) &&
+            routeTable.routeEntries().anyMatch(r -> routeMatchesUnroute(r.route(), unroute));
+
+        if (unrouted)
+        {
+            routesLayout.lock();
+
+            routeTableRO.wrap(routesBuffer, 0, routesBufferCapacity);
+            routeTableRW.wrap(routesBuffer, 0, routesBufferCapacity);
+
+            routeTableRW.routeEntries(b ->
+            {
+                routeTableRO.routeEntries().forEach(
+                    e -> b.item(copyRouteEntry(e.routeSize(), e.route()))
+                );
+            });
+
+            routesLayout.unlock();
+        }
+        return unrouted;
     }
 
+    Object r;
     public <R> R resolve(
         final long authorization,
         MessagePredicate filter,
         MessageFunction<R> mapper)
     {
-        R mapped = null;
-
-        MessagePredicate secure = (m, b, i, l) ->
+        // anyMatch matchFirst firstMatch(
+        r = null;
+        RouteTableFW routeTable = routeTableRO.wrap(routesBuffer, 0, routesBufferCapacity);
+        routeTable.routeEntries().forEach(re ->
         {
-            RouteFW route = routeRO.wrap(b, i, i + l);
-            final long routeAuthorization = route.authorization();
-            return (authorization & routeAuthorization) == routeAuthorization;
+            RouteFW candidate = re.route();
+            final long routeAuthorization = candidate.authorization();
+            if ((authorization & routeAuthorization) == routeAuthorization)
+            {
+                final int typeId = candidate.typeId();
+                final DirectBuffer buffer = candidate.buffer();
+                final int offset = candidate.offset();
+                final int length = candidate.sizeof();
+                r = mapper.apply(typeId, buffer, offset, length);
+            }
+        });
+
+        return r == null ? null : (R) r;
+    }
+
+    private static Consumer<RouteEntryFW.Builder> copyRouteEntry(
+        int size,
+        RouteFW route)
+    {
+        return routeEntryBuilder ->
+        {
+            routeEntryBuilder.routeSize(size);
+            routeEntryBuilder.route(routeBuilder -> copyRoute(routeBuilder, route));
         };
-        final MessagePredicate secureFilter = secure.and(filter);
+    }
 
-        final Optional<RouteFW> optional = routes.stream()
-              .filter(r -> secureFilter.test(r.typeId(), r.buffer(), r.offset(), r.limit()))
-              .findFirst();
+    private static void copyRoute(
+        RouteFW.Builder b,
+        RouteFW route)
+    {
+        final OctetsFW extension = route.extension();
 
-        if (optional.isPresent())
-        {
-            final RouteFW route = optional.get();
+        b.correlationId(route.correlationId())
+         .role(rb -> rb.set(route.role()))
+         .source(route.source())
+         .sourceRef(route.sourceRef())
+         .target(route.target())
+         .targetRef(route.targetRef())
+         .authorization(route.authorization())
+         .extension(
+             extension.buffer(),
+             extension.offset(),
+             extension.limit());
+    }
 
-            mapped = mapper.apply(route.typeId(), route.buffer(), route.offset(), route.limit());
-        }
-
-        return mapped;
+    private static boolean routeMatchesUnroute(
+        RouteFW route,
+        UnrouteFW unroute)
+    {
+        return route.role() == unroute.role() &&
+        unroute.source().equals(route.source()) &&
+        unroute.sourceRef() == route.sourceRef() &&
+        unroute.target().equals(route.target()) &&
+        unroute.targetRef() == route.targetRef() &&
+        unroute.authorization() == route.authorization();
     }
 }
