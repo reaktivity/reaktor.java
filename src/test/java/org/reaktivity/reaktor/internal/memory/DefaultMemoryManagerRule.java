fe1e212870604d0e1d665f4f823809728cf52e3a
David Witherspoon <david.witherspoon@kaazing.com>
2018-02-01 11:48:03 -0800

    Added license to DefaultMemoryManager, moved to internal package

--- src/test/java/org/reaktivity/reaktor/internal/memory/DefaultMemoryManagerRule.java
+++ src/test/java/org/reaktivity/reaktor/internal/memory/DefaultMemoryManagerRule.java
@@ -1,4 +1,19 @@
-package org.reaktivity.reaktor.test;
+/**
+ * Copyright 2016-2017 The Reaktivity Project
+ *
+ * The Reaktivity Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package org.reaktivity.reaktor.internal.memory;
 
 import java.io.File;
 import java.nio.file.Path;
@@ -9,8 +24,6 @@ import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 import org.reaktivity.nukleus.buffer.MemoryManager;
-import org.reaktivity.reaktor.internal.memory.DefaultMemoryManager;
-import org.reaktivity.reaktor.internal.memory.MemoryLayout;
 
 public class DefaultMemoryManagerRule implements TestRule
 {
